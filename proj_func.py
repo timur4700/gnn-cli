774855214a9fc1b879f369976f118ff42ae6bc07
timur4700 <timur4700@gmail.com>
2026-07-14 21:05:24 +0200

    bug fix

--- proj_func.py
+++ proj_func.py
@@ -18,7 +18,7 @@ from train.utils import init_train_config
 
 def make(prj_name: str, args):
 
-            prj_name = prj_name.strip()
+
 
             if prj_name in state.APP_STATE.projects.projects:
                 raise FileExistsError((f'The project with the name ({prj_name}) already exists.\nPlease choose another name.'))
@@ -26,6 +26,8 @@ def make(prj_name: str, args):
 
             if not prj_name:
                 prj_name = Path(func.looped_input('Enter the project name\n', str))
+
+            prj_name = prj_name.strip()
                 
             proj_config = func_proj.configure_new_project(prj_name, state.APP_STATE)
 
@@ -56,7 +58,7 @@ def make(prj_name: str, args):
 
                 graph_prep_config = graph_config.configure_graph_preparation(proj_config)
 
-                train_data, test_data, graph_prep_config = graph_prep.main(data, 
+                train_data, test_data, graph_prep_config = graph_prep.main_multi(data, 
                                                         proj_config, 
                                                         graph_prep_config)
 
